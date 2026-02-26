ec3a189945c0b5c67a3f202c87237ae8c5f6601c
Brille3000 <Brille31@web.de>
2026-02-26 10:50:07 +0000

    Variable deklariert

---
+++ com/cc/java/Bestellung.java
@@ -0,0 +1,20 @@
+package com.cc.java;
+
+public class Bestellung {
+
+
+public static void main(String[] args) {
+
+    //Variablen deklarieren
+    String food = "noch keinen Bürger gewählt";
+    String drink = "noch keinen Drink gewählt";
+    String itemNumStr;
+    String flag;
+    
+    System.out.println(food);
+    System.out.println(drink); 
+      
+
+}
+
+}
